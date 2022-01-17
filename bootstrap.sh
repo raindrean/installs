41fd4ccbdc0177b29ab515b12c03409a36aa153b
raindrean <yangzun04@gmail.com>
2022-01-17 13:07:24 -0800

    modify bootstrap.sh: check repo commit, copy SHLL_VARS_template content

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,11 @@ if [ ! -d ~/.local ]; then
 	mkdir -p ~/.local
 fi
 
+willCopySHELL_VARS_template=
+if [[ ! -f $SHELL_VAR_FILE ]]; then
+	willCopySHELL_VARS_template=1
+fi
+
 touch $SHELL_VAR_FILE
 source $SHELL_VAR_FILE
 
@@ -56,14 +61,47 @@ if [[ -z $CONFIG_ROOT_DIR ]]; then
 	CONFIG_ROOT_DIR=$config_root
 fi
 
+repoURL=git@github.com:raindrean/ztools.git
 if [ ! -d "$CONFIG_ROOT_DIR/ztools" ]; then
-	git clone git@github.com:raindrean/ztools.git $CONFIG_ROOT_DIR/ztools
+	git clone $repoURL $CONFIG_ROOT_DIR/ztools
 else
 	CUR=$(pwd)
 	cd "$CONFIG_ROOT_DIR/ztools"
 	git pull
+	if [[ $? != 0 ]]; then
+		git status
+		echo "local repo has conflicts, aborted the bootstrapping..."
+		exit 0
+	fi
+	latest_remote_commit=$(git ls-remote $repoURL HEAD)
+	if [[ $? != 0 ]]; then
+		echo "getting remote latest HEAD commid id failed... exit"
+		exit 0
+	fi
+	latest_remote_commitId=$(echo "$latest_remote_commit" | cut -d$'\t' -f1)
+	if [[ $? != 0 ]]; then
+		echo "parsing remote latest HEAD commid id failed... exit"
+		exit 0
+	fi
+	latest_local_commitId=$(git log --format="%H" -n 1)
+	if [[ $? != 0 ]]; then
+		echo "getting local latest commid id failed... exit"
+		exit 0
+	fi
+	if [[ $latest_remote_commitId != $latest_local_commitId ]]; then
+		echo "the remote commid id and local commit id are not the same"
+		echo "Please check local repo: $CONFIG_ROOT_DIR/ztools"
+		echo "and remote repo: https://github.com/raindrean/ztools or via ssh: $repoURL"
+		exit 0
+	fi
+
+	# copy variable names to
+	if [[ $willCopySHELL_VARS_template == "1" ]]; then
+    echo "CONFIG_ROOT_DIR=$config_root" >> $SHELL_VAR_FILE
+    cat SHELL_VARS_template.sh >>$SHELL_VAR_FILE
+    echo "" >> $SHELL_VAR_FILE
+  fi
 fi
 # TODO: 如果zshrc里面已有这句话，则不写入
 echo "source $SHELL_VAR_FILE" >> $SHELL_PROFILE
 echo "source $CONFIG_ROOT_DIR/ztools/init.sh" >> $SHELL_PROFILE
-
